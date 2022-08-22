d443cc49f97ad7992b9a22a44595561e51d49ac5
Amilcar-junior <amilcarjuniordev@gmail.com>
2022-08-22 10:23:10 -0300

    create new project

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+function SignIn (){ //Todo componente deve começar com letra maiúscula
+    return (
+        <View>
+            <Text> Ola mundo de merda</Text>
+        </View>
+    )
+}
+export default SignIn;
