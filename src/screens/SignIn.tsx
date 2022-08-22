fc199975033a0a7ded48e1844090e9ef9b106bd3
Amilcar-junior <amilcarjuniordev@gmail.com>
2022-08-22 12:15:50 -0300

    create style satus bar

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import {View,Text} from 'react-native';
+import {VStack, Heading} from 'native-base';
 function SignIn (){ //Todo componente deve começar com letra maiúscula
     return (
-        <View style={{}}>
-            <Text> Ola mundo de merda</Text>
-        </View>
+        <VStack flex={1} alignItems="center" bg="gray.700" px={8} pt={24}>
+            <Heading color="gray.100" fontSize="xl" mt={20} >
+                Acesse sua conta  
+            </Heading>
+        </VStack>
     )
 }
 export default SignIn;
